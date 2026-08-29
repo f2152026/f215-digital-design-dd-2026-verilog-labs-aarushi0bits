decea2fc2763f9fed997859a131dace7d6059cef
aarushi balaji <f20251193@pilani.bits-pilani.ac.in>
2026-08-29 08:41:59 +0000

    lab01 task2

--- labs/lab01/task2/FA_Gate.v
+++ labs/lab01/task2/FA_Gate.v
@@ -26,10 +26,10 @@ module FA_Gate(
 );
   wire ps, pc1, pc2;
 
-  xor #(2,3) (ps,  a,   b); // rise delay=2, fall delay=3
-  and #(2,3) (pc1, a,   b);
-  xor #(2,3) (sum, cin, ps);
-  and #(2,3) (pc2, cin, ps);
-  or #(2,3) (cout, pc1, pc2);
+  xor #(1,2) (ps,  a,   b); // rise delay=2, fall delay=3
+  and #(1,2) (pc1, a,   b);
+  xor #(1,2) (sum, ps, cin);
+  and #(1,2) (pc2, ps, cin);
+  or #(1,2) (cout, pc1, pc2);
 
 endmodule
